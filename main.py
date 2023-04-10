188c0c4b467fa578565e3974d779539f16f96512
Melih <melih@Melihs-MacBook-Air.local>
2023-04-10 19:36:49 +0300

    Make the code more readable.

--- main.py
+++ main.py
@@ -1,56 +1,69 @@
 import csv
 
+INPUT_FILE = "sample/sample_input.csv"
+OUTPUT_FILE = "sample/sample_output.csv"
+
 def preprocess_input(input_file):
-    votes = {}
-    seats = {}
-    parties = []
+    votes, seats, parties = {}, {}, []
+
     with open(input_file, 'r') as file:
         reader = list(csv.reader(file)) # read the file into a list of lists
-        header = reader[0] # get the header
-        parties = header[2:] # get the parties
+        parties = reader[0][2:] # get the parties
+
         for line in reader[1:]:
             if len(line[2:]) != len(parties): 
                 print("Please check the input file. The number of parties is not consistent for all constituencies.")
                 exit()
+
             votes[line[0]] = [int(i) for i in line[2:]]
             seats[line[0]] = int(line[1])
+
     return votes, seats, parties
 
-def main():
-    input_file = "sample/sample_input.csv"
-    output_file = "sample/sample_output.csv"
-
-    votes, seats, parties = preprocess_input(input_file)
-    result = []
+def calculate_seats(votes, seats, parties):
+    results = []
     total = len(parties) * [0]
 
-    for key in votes.keys():
-        if sum(votes[key]) == 0:
-            print("No votes were found for " + key)
+    for constituency in votes.keys():
+        if sum(votes[constituency]) == 0:
+            print("No votes were found for " + constituency)
             continue
-        result.append({})
-        result[len(result) - 1]['constituency'] = key
+
         rates = len(parties) * [1]
-        for i in range(seats[key]):
-            tmp = [(votes[key][i] / rates[i]) for i in range(len(parties))]
+
+        for i in range(seats[constituency]):
+            tmp = [(votes[constituency][i] / rates[i]) for i in range(len(parties))]
             rates[tmp.index(max(tmp))] += 1
+
+        data_dict = {"constituency": constituency}
         for i in range(len(parties)):
-            result[len(result) - 1][parties[i]] = rates[i] - 1
+            data_dict[parties[i]] = rates[i] - 1
             total[i] += (rates[i] - 1)
-        print(key + " seats have been calculated successfully.")
+        results.append(data_dict)
+
+        print(constituency + " seats have been calculated successfully.")
         print([rates[i] - 1 for i in range(len(rates))])
 
-    result.append({})
-    result[len(result)-1]['constituency'] = "total"
+    total_seats = {"constituency": "total"}
     for i in range(len(parties)):
-        result[len(result) - 1][parties[i]] = total[i]
+        total_seats[parties[i]] = total[i]
+    results.append(total_seats)
 
-    with open(output_file, 'w', newline='') as file:
-        field_names = ['constituency'] + parties
+    return results
+
+def main():
+    input_file = INPUT_FILE
+    output_file = OUTPUT_FILE
+
+    votes, seats, parties = preprocess_input(input_file)
+    seats = calculate_seats(votes, seats, parties)
+    
+    with open(output_file, "w", newline="") as file:
+        field_names = ["constituency"] + parties
         writer = csv.DictWriter(file, fieldnames=field_names)
         writer.writeheader()
-        for result in result:
-            writer.writerow(result)
+        for constituency in seats:
+            writer.writerow(constituency)
 
 if __name__ == "__main__":
     main()
